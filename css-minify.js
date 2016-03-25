18491acd73f811db86a61ec2b5dbf88809784531
Mark McCann <mark.mccann@usaa.com>
2016-03-25 14:43:11 -0500

    basic functionality created

--- css-minify.js
+++ css-minify.js
@@ -22,6 +22,10 @@ var cssMinify = (function(){
      * default settings for alphabetizer
      */
     var defaults = {
+        lastSemicolon: false,
+        comments: true,
+        leadingZeros: true,
+        zeroUnits: true
     }
 
     /**
@@ -30,7 +34,20 @@ var cssMinify = (function(){
     return function( styles, options ) {
         // set minifier settings
         var settings = extend( defaults, options || {} );
-
+        // remove comments if setting is set
+        if( settings.comments ) styles = styles.replace(/\/\*((?!\*\/)(.|\n))*\*\//g, '');
+        // else remove space before and after comment
+        else styles = styles.replace( /(\*\/)\s*/g, '$1' );
+        // remove all unnecessary spaces
+        styles = styles.replace(/\s*([:;,{}])\s*/g,'$1');
+        // remove final semi-colon if setting set
+        if( settings.lastSemicolon ) styles = styles.replace( /;((?:\/\*((?!\*\/)(.|\n))*\*\/)*})/g, '$1' );
+        // remove leading zero if setting set
+        if( settings.leadingZeros ) styles = styles.replace( /0(\.\d+)/g, '$1' );
+        // remove leading zero if setting set
+        if( settings.zeroUnits ) styles = styles.replace( /0(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/g, '0' );
+        // return the minified css
+        return styles.trim();
     }
 
 })();
