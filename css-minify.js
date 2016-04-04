01d311631a399546cffb118b3d5ea4930d2d36ee
Mark McCann <mark.mccann@usaa.com>
2016-04-04 17:05:56 -0500

    broke parts of css apart to prevent problems with special chars in strings and urls, removed comments setting

--- css-minify.js
+++ css-minify.js
@@ -22,8 +22,7 @@ var cssMinify = (function(){
      * default settings for minifier
      */
     var defaults = {
-        lastSemicolon: true,
-        comments: true,
+        lastSemiColon: true,
         leadingZeros: true,
         zeroUnits: true
     }
@@ -34,20 +33,81 @@ var cssMinify = (function(){
     return function( styles, options ) {
         // set minifier settings
         var settings = extend( defaults, options || {} );
-        // remove comments if setting is set
-        if( settings.comments ) styles = styles.replace(/\/\*((?!\*\/)(.|\n))*\*\//g, '');
-        // else remove space before and after comment
-        else styles = styles.replace( /(\*\/)\s*/g, '$1' );
-        // remove all unnecessary spaces
-        styles = styles.replace(/\s*([:;,{}])\s*/g,'$1');
-        // remove final semi-colon if setting set
-        if( settings.lastSemicolon ) styles = styles.replace( /;((?:\/\*((?!\*\/)(.|\n))*\*\/)*})/g, '$1' );
-        // remove leading zero if setting set
-        if( settings.leadingZeros ) styles = styles.replace( /0(\.\d+)/g, '$1' );
-        // remove leading zero if setting set
-        if( settings.zeroUnits ) styles = styles.replace( /0(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/g, '0' );
-        // return the minified css
-        return styles.trim();
+        // minify the styles
+        return styles
+            // remove all comments
+            .replace(/\/\*((?!\*\/)(.|\n))*\*\//g, '')
+            // rules with nested styles
+            .replace(/[^{}]*{([^{}]*{[^{}]*([("'].*['")])*[^{}]*}\s*)*}/g, function(query){
+                return query
+                    // query selector
+                    .replace(/^[^{]*{/, function(selector){
+                        return selector
+                            // remove spaces around open bracket
+                            .replace(/\s*{\s*$/,'{')
+                            // remove spaces from start
+                            .replace(/^\s*/,'');
+                    })
+                    // remove spaces around closing bracket
+                    .replace(/\s*}\s*$/, '}');
+            })
+            // standard rules
+            .replace(/[^{}]*{[^{}]*([("'].*['")])*[^{}]*}/g, function(rule) {
+                return rule
+                    // the selector
+                    .replace(/^[^{]*{/, function(selector){
+                        return selector
+                            // remove spaces around open bracket
+                            .replace(/\s*{\s*$/,'{')
+                            // remove spaces from start
+                            .replace(/^\s*/,'')
+                            // remove spaces around combinators
+                            .replace(/\s*([>~+,])\s*/g,'$1')
+                            // reduce decendants to single space
+                            .replace(/ *\n+ */g, ' ');
+                    })
+                    // the contents
+                    .replace(/{[^{}]*([("'](.|\n)*['")])*[^{}]*}\s*$/, function(contents){
+                        return contents
+                            // each declaration
+                            .replace(/([\w\s-]*):(([^;'"(}])*(\(["']?[^'")]*['"]?\)|["'][^'"]*['"])?([^;}])*);?\s*/g, function(declaration, property, value){
+                                return declaration
+                                    // property
+                                    .replace(property, function(prop){
+                                        return prop
+                                            // remove spaces from end
+                                            .replace(/\s*$/,'')
+                                            // remove spaces from start
+                                            .replace(/^\s*/,'');
+                                    })
+                                    // value
+                                    .replace(value, function(val){
+
+                                        return val
+                                            // remove spaces from end
+                                            .replace(/\s*$/,'')
+                                            // remove spaces from start
+                                            .replace(/^\s*/,'')
+                                            // remove spaces around certain characters
+                                            .replace(/\s*([,])\s*/g, '$1')
+                                            // remove leading zero if setting set
+                                            .replace( /0(\.\d+)/g, (settings.leadingZeros?'':'0')+'$1' )
+                                            // remove units on zeros if setting set
+                                            .replace( /0(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/g, '0'+(settings.zeroUnits?'':'$1') );
+                                    })
+                                    // remove spaces from end
+                                    .replace(/\s*$/,'')
+                                    // remove spaces from start
+                                    .replace(/^\s*/,'');
+                            })
+                            // replace any new lines with single space
+                            .replace( / *\n+ */g, ' ')
+                            // remove final semicolon if set
+                            .replace( /;\s*}\s*$/g, (settings.lastSemiColon?'':';')+'}')
+                    })
+            })
+            // remove any trailing spaces
+            .trim();
     }
 
 })();
